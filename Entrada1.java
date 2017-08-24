9df52ecca57dac507e9d449eb7d41906a08b1894
danielroa98 <a01021960@itesm.mx>
2017-08-24 12:57:16 -0500

    Subido a la final de la clase del 24/08/2017

--- Entrada1.java
+++ Entrada1.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.util.Scanner; //necesario para poder leer datos escritos desde el teclado
 
 public class Entrada1
 {
   public static void main (String[] args)
   {
-    String nombre;
+    String nombre;//, apellido;
 
-    Scanner stdIn =new Scanner(System.in);
+    Scanner stdIn =new Scanner(System.in);/*********/
+    //Scanner atdIn = new Scanner(System.in);
 
-    nombre = stdIn.nextLine();
-
-    System.out.println(nombre);
+    nombre = stdIn.nextLine();/**********/
+    //apellido = atdIn.nextLine();
+    System.out.println("Hola "+nombre);
   }
 }
