406a90f6af3ce3b3cf75b29fc43f9f42b59453f2
danielroa98 <a01021960@itesm.mx>
2017-08-24 12:47:28 -0500

    Subido a la final de la clase del 24/08/2017

---
+++ Entrada1.java
@@ -0,0 +1,4 @@
+public class Entrada1
+{
+  public static void main (String[] args)
+  {
--- Variables2.java
+++ Variables2.java
@@ -4,16 +4,24 @@ public class Variables2
   {
     char inicialNombre, inicialApellido;
     char codigoControl;
+    //int, long
+    //double, float
+    //char, String
 
     inicialNombre = 'D';
     inicialApellido = 'R';
 
     codigoControl = '\n'; // \t
 
+    String texto = "Mi nombre es ";
+
     System.out.println("Hola "+inicialNombre);
     System.out.println("Codigo: "+codigoControl);
     System.out.println("\"Hola de  \t nuevo \r "+inicialApellido);
     System.out.println("\\");
+
+    System.out.println("Los caracteres de texto son "+texto.length());
+    System.out.println("El caracter en 5 es "+texto.charAt(12));
 
   }
 }
