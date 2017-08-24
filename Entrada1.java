0e7ceeab0142e47411c6bf840df73ba87ac3856a
danielroa98 <a01021960@itesm.mx>
2017-08-24 12:50:56 -0500

    Subido a la final de la clase del 24/08/2017

--- Entrada1.java
+++ Entrada1.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Entrada1
 {
   public static void main (String[] args)
   {
+    String nombre;
+
+    Scanner stdIn =new Scanner(System.in);
+
+    nombre = stdIn.nextLine();
+
+    System.out.println(nombre);
+  }
+}
