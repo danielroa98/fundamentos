f8027a18c4b49733aecb72f77c87017cedaf9dc5
danielroa98 <a01021960@itesm.mx>
2017-08-28 12:03:39 -0500

    Trabajo del 28 de agosto

---
+++ Metodo1.java
@@ -0,0 +1,18 @@
+/*
+Ejemplo de uso de métodos
+Daniel Roa
+A01021960
+28 de Agosto del 2017
+*/
+public class Metodo1
+{
+  public static void main(String[] args)
+  {
+    imprime();
+    imprime();
+  }
+    public static void imprime()
+    {
+      System.out.printf("Hola desde el metodo. %n");
+    }
+}
