ddf32c0c441e7efa5d4cfdd259f8a710afd3d88e
danielroa98 <a01021960@itesm.mx>
2017-08-31 12:49:15 -0500

    Hecho en la clase del 31 de agosto del 2017

--- Condiciones1.java
+++ Condiciones1.java
@@ -9,6 +9,7 @@ public class Condiciones1
   public static void main(String[] args)
   {
     int temperatura;
+    String salida;
     Scanner lectura = new Scanner(System.in);
 
     System.out.println("Escriba la temperatura del auto.");
@@ -18,14 +19,24 @@ public class Condiciones1
     // diferente se escribe !=
     temperatura = lectura.nextInt();
 
-    if(temperatura>=110 || temperatura < -10)
+    if(temperatura < -10)
+    {
+      System.out.println("Temperatura muy baja.");
+    }
+    else if(temperatura >= -10 && temperatura <= 110)
+    {
+      System.out.println("Todo OK. Puedes continuar.");
+    }
+    if(temperatura>110)
     {
       System.out.println("Temperatura muy alta, detente por favor");
       System.out.println("Revisa el nivel de aceite.");
     }
-    else
-    {
-      System.out.println("Todo OK. Puedes continuar.");
-    }
+
+    lectura.nextLine();
+    System.out.println("Escribe s para salir");
+    salida = lectura.nextLine();
+    if(salida.equals("s"))
+    {System.out.println("Adios!");}
   }
 }
