b410202086c75181e99963b4a0f5941760b0b91a
danielroa98 <a01021960@itesm.mx>
2017-10-02 12:05:35 -0500

    Version modificada el 2 de Octubre

--- For2.java
+++ For2.java
@@ -2,10 +2,10 @@ public class For2
 {
   public static void main(String[] args)
   {
-    for (int i=1; i<=5; i++)
+    for (int i=1; i<=9; i++)
     {
-      System.out.println("Estoy en la i="+i);
-      for (int j=1; j<=4; j++)
+      //System.out.println("Estoy en la i="+i);
+      for (int j=9; j>=i; j--)
       {
           System.out.printf(" (%d,%d) \t", i,j);
       }
